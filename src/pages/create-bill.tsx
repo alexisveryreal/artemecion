fccd4ca1abbf717d1a69d3bdc9511e4dda5d2399
alexisveryreal <alexmaple20@gmail.com>
2023-02-02 15:22:20 -0800

    modify db again to be more simpler 404 page finished design for bill table modify create-bill page

--- src/pages/create-bill.tsx
+++ src/pages/create-bill.tsx
@@ -1,24 +1,21 @@
 import { ArrowPathIcon } from "@heroicons/react/20/solid";
+import type { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { useController } from "react-hook-form";
 import { z } from "zod";
 import { Form, useZodForm } from "../components/forms/Form";
 import { FormInput } from "../components/forms/FormInput";
 import { FormOptionGroup } from "../components/forms/FormOptionGroup";
-import { FormSelect } from "../components/forms/FormSelect";
-import { FormTextarea } from "../components/forms/FormTextarea";
 import { SuccessModal } from "../components/SuccessModal";
 import { Button } from "../components/ui/Button";
 import type { Options } from "../components/ui/OptionGroup";
-import { OptionGroup } from "../components/ui/OptionGroup";
+import { getServerAuthSession } from "../server/auth";
 import { api } from "../utils/api";
 import { cn } from "../utils/cn";
 
 // this schema is re-used in bill.router.ts
 export const billCreateSchema = z.object({
   name: z.string().min(1, "Name must have at least one character"),
-  description: z.string().nullable(),
   amount: z.coerce
     .number({
       invalid_type_error: "Please enter only numbers",
@@ -27,9 +24,7 @@ export const billCreateSchema = z.object({
     .min(1, "Amount must be greater than 0"),
   type: z.enum(["OneTime", "Recurring"]),
   url: z.string().url("Please enter a valid url"),
-  recurringType: z.enum(["Monthly", "Yearly", "Quaterly", "None"]),
-  recurringDate: z.date().nullable(),
-  oneTimeDate: z.date().nullable(),
+  billDate: z.coerce.date(),
 });
 
 const options: Options[] = [
@@ -43,34 +38,16 @@ const options: Options[] = [
   },
 ];
 
-const ReccuringMap: Record<
-  z.infer<typeof billCreateSchema>["recurringType"],
-  string
-> = {
-  Monthly: "Monthly",
-  Quaterly: "Quaterly",
-  Yearly: "Yearly",
-  None: "None",
-};
-
 const CreateBillPage = () => {
   const form = useZodForm({
     schema: billCreateSchema,
     defaultValues: {
       type: "Recurring",
-      recurringType: "None",
-      recurringDate: null,
-      oneTimeDate: null,
     },
   });
 
-  const type = form.watch("type");
-  const recurringType = form.watch("recurringType");
-
   const [modalOpen, setModalOpen] = useState(false);
   const router = useRouter();
-
-  // const context = api.useContext()
 
   const { mutate: createBill, isLoading } = api.bill.create.useMutation({
     onSuccess: () => {
@@ -131,17 +108,7 @@ const CreateBillPage = () => {
                           The amount of this specific bill
                         </p>
                       </div>
-                      <div className="sm:col-span-6">
-                        <FormTextarea
-                          rows={3}
-                          label="Description"
-                          cornerHint="Optional"
-                          {...form.register("description")}
-                        />
-                        <p className="mt-2 text-sm text-zinc-500">
-                          Write anything about the bill here!
-                        </p>
-                      </div>
+
                       <div className="sm:col-span-6">
                         <FormOptionGroup
                           label="Bill Type"
@@ -150,7 +117,7 @@ const CreateBillPage = () => {
                           options={options}
                         />
                       </div>
-                      <div className="sm:col-span-6">
+                      <div className="sm:col-span-3">
                         <FormInput
                           type="text"
                           label="Site Url"
@@ -160,59 +127,16 @@ const CreateBillPage = () => {
                           Please include https://
                         </p>
                       </div>
-                      <div
-                        className={cn(
-                          type !== "Recurring" ? "hidden" : "block",
-                          "sm:col-span-3"
-                        )}
-                      >
-                        <FormSelect
-                          label="Recurring Type"
-                          {...form.register("recurringType")}
-                        >
-                          {billCreateSchema.shape.recurringType.options.map(
-                            (op) => (
-                              <option key={op} value={op}>
-                                {ReccuringMap[op]}
-                              </option>
-                            )
-                          )}
-                        </FormSelect>
-                      </div>
-                      <div
-                        className={cn(
-                          recurringType === "None" || type === "OneTime"
-                            ? "hidden"
-                            : "block",
-                          "sm:col-span-3"
-                        )}
-                      >
-                        <FormInput
-                          type="datetime-local"
-                          label="Recurring Date"
-                          {...form.register("recurringDate", {
-                            valueAsDate: true,
-                          })}
-                        />
 
-                        <p className="mt-2 text-sm text-zinc-500">
-                          Note: time section will be what we base off to notify
-                          you
-                        </p>
-                      </div>
-                      <div
-                        className={cn(
-                          type === "Recurring" ? "hidden" : "block",
-                          "sm:col-span-3"
-                        )}
-                      >
+                      <div className={cn("sm:col-span-3")}>
                         <FormInput
-                          type="datetime-local"
-                          label="One Time Date"
-                          {...form.register("oneTimeDate", {
-                            valueAsDate: true,
-                          })}
+                          type="date"
+                          label="Bill Date"
+                          {...form.register("billDate")}
                         />
+                        <p className="mt-2 text-sm text-zinc-500">
+                          What day was this bill paid/will be paid
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -236,7 +160,6 @@ const CreateBillPage = () => {
                         <ArrowPathIcon className="mr-2 h-4 w-4 animate-spin" />
                       )}
                       {isLoading ? "Saving" : "Save"}
-                      {/* Save */}
                     </Button>
                   </div>
                 </div>
@@ -253,7 +176,7 @@ const CreateBillPage = () => {
         }}
         onClose={(option) => setModalOpen(option)}
         open={modalOpen}
-        text="We will notify you shortly via email"
+        text="Congrats, visit the dashboard to see your bill"
         title="Created Bill"
       />
     </div>
@@ -261,3 +184,23 @@ const CreateBillPage = () => {
 };
 
 export default CreateBillPage;
+
+// if they aren't logged in re-direct to landing
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,31 +1,18 @@
-import { BanknotesIcon } from "@heroicons/react/20/solid";
-import { useSession } from "next-auth/react";
+import { PlusIcon } from "@heroicons/react/20/solid";
+import { BanknotesIcon } from "@heroicons/react/24/outline";
+import type { GetServerSideProps } from "next";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { Button } from "../components/ui/Button";
+import { getServerAuthSession } from "../server/auth";
 import type { RouterOutputs } from "../utils/api";
 import { api } from "../utils/api";
-import { cn } from "../utils/cn";
+import { formatDate } from "../utils/date";
 const DashboardPage = () => {
-  const { status } = useSession();
-  const router = useRouter();
-
   const { data: bills } = api.bill.getUserBills.useQuery(undefined, {
     refetchOnWindowFocus: false,
     refetchInterval: false,
     refetchOnReconnect: false,
   });
-
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      void router.replace("/");
-    }
-  }, [router, status]);
-
-  if (status === "unauthenticated") {
-    return <h1>Oops please login to see this page :D </h1>;
-  }
 
   return (
     <div className="min-h-screen py-10">
@@ -39,16 +26,29 @@ const DashboardPage = () => {
       <main>
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="px-4 py-8 sm:px-0">
-            {bills && (
-              // <ul role="list" className="divide-y divide-zinc-200">
-              //   {bills.map((bill) => (
-              //     <li key={bill.id} className="px-4 py-4 sm:px-0">
-              //       {bill.name}
-              //     </li>
-              //   ))}
-              // </ul>
-              <BillTable bills={bills} />
+            {bills && bills.length === 0 && (
+              <div className="text-center">
+                <BanknotesIcon className="mx-auto h-12 w-12 text-zinc-400" />
+                <h3 className="mt-2 text-sm font-medium text-gray-900">
+                  No bills
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">
+                  Get started by tracking a bill
+                </p>
+                <div className="mt-6">
+                  <Link href="/create-bill">
+                    <Button variant="default">
+                      <PlusIcon
+                        className="-ml-1 mr-2 h-5 w-5"
+                        aria-hidden="true"
+                      />
+                      New Bill
+                    </Button>
+                  </Link>
+                </div>
+              </div>
             )}
+            {bills && bills.length > 0 && <BillTable bills={bills} />}
           </div>
         </div>
       </main>
@@ -60,12 +60,6 @@ export default DashboardPage;
 
 type BillTableProps = {
   bills: RouterOutputs["bill"]["getUserBills"];
-};
-
-const statusStyles = {
-  success: "bg-green-100 text-green-800",
-  processing: "bg-yellow-100 text-yellow-800",
-  failed: "bg-gray-100 text-gray-800",
 };
 
 const BillTable = ({ bills }: BillTableProps) => {
@@ -80,43 +74,45 @@ const BillTable = ({ bills }: BillTableProps) => {
         </div>
       </div>
       <div className="-mx-4 mt-8 flex flex-col sm:-mx-6 md:mx-0">
-        <table className="min-w-full divide-y divide-gray-300">
-          <thead>
+        <table className="min-w-full divide-y divide-zinc-300">
+          <thead className="bg-violet-50">
             <tr>
               <th
                 scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
+                className="sticky top-14 z-10 bg-violet-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 md:pl-8"
               >
                 Name
               </th>
               <th
                 scope="col"
-                className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
+                className="sticky top-14 z-10 hidden bg-violet-50 bg-opacity-75 py-3.5 px-3 text-right text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
               >
                 Amount
               </th>
               <th
                 scope="col"
-                className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
+                className="sticky top-14 z-10 hidden bg-violet-50 bg-opacity-75 py-3.5 px-3 text-right text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
               >
-                Type
+                Category
               </th>
               <th
                 scope="col"
-                className="py-3.5 pl-3 pr-4 text-right text-sm font-semibold text-gray-900 sm:pr-6 md:pr-0"
+                className="sticky top-14 z-10 bg-violet-50 bg-opacity-75 py-3.5 pl-3 pr-4 text-right text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pr-6 md:pr-8"
               >
-                Description
+                Date
               </th>
             </tr>
           </thead>
           <tbody>
             {bills.map((bill) => (
               <tr key={bill.id} className="border-b border-gray-200">
-                <td className="py-4 pl-4 pr-3 text-sm sm:pl-6 md:pl-0">
+                <td className="py-4 pl-4 pr-3 text-sm sm:pl-6 md:pl-8">
                   <div className="font-medium text-gray-900">
                     <Link
                       href={bill.url ?? "#"}
                       className="capitalize underline-offset-4 hover:underline"
+                      target={"_blank"}
+                      rel="noreferrer"
                     >
                       {bill.name}
                     </Link>
@@ -132,31 +128,33 @@ const BillTable = ({ bills }: BillTableProps) => {
                 <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">
                   {bill.type.split(/(?=[A-Z])/).join(" ")}
                 </td>
-                <td className="py-4 pl-3 pr-4 text-right text-sm text-gray-500 sm:pr-6 md:pr-0">
-                  {bill.description}
+                <td className="py-4 pl-3 pr-4 text-right text-sm text-gray-500 sm:pr-6 md:pr-8">
+                  {formatDate(bill.createdAt)}
                 </td>
               </tr>
             ))}
           </tbody>
-          {/* <tfoot>
-          <tr>
-            <th
-              scope="row"
-              colSpan={3}
-              className="hidden pl-6 pr-3 pt-4 text-right text-sm font-semibold text-gray-900 sm:table-cell md:pl-0"
-            >
-              Total
-            </th>
-            <th scope="row" className="pl-4 pr-3 pt-4 text-left text-sm font-semibold text-gray-900 sm:hidden">
-              Total
-            </th>
-            <td className="pl-3 pr-4 pt-4 text-right text-sm font-semibold text-gray-900 sm:pr-6 md:pr-0">
-              $4,485.00
-            </td>
-          </tr>
-        </tfoot> */}
         </table>
       </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
 };
