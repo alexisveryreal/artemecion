b80e95c5f982e7024bff146992cf8ca3cf5d5b3d
alexisveryreal <alexmaple20@gmail.com>
2024-08-16 11:16:22 -0700

    worked out the keyboard shortcuts hehe added delete modal need to implement delete action and will trigger that

--- src/app/(platform)/(dashboard)/dashboard/data-table-row-actions.tsx
+++ src/app/(platform)/(dashboard)/dashboard/data-table-row-actions.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { type Row } from "@tanstack/react-table";
@@ -10,10 +11,12 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
-  // DropdownMenuShortcut,
+  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useOS } from "@/hooks/useOS";
 
+import { DeleteDialog } from "./delete-dialog";
 import { billSchema } from "./schema";
 
 interface DataTableRowActionsProps<TData> {
@@ -25,35 +28,65 @@ export function DataTableRowActions<TData>({
 }: DataTableRowActionsProps<TData>) {
   const bill = billSchema.parse(row.original);
   const router = useRouter();
+  const os = useOS();
+  const [open, setOpen] = useState(false);
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log("OS IS: ", os);
+    console.log("Code is: ", e.code);
+    if ((e.metaKey || e.ctrlKey) && e.code === "Backspace") {
+      console.log("HELLO WORLD");
+      // modal here
+    }
+    if ((e.metaKey || e.ctrlKey) && e.code === "KeyE") {
+      console.log("EDIT");
+      router.push(`/edit-bill/${bill.id}`);
+    }
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          >
+            <DotsHorizontalIcon className="h-4 w-4" />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          align="end"
+          className="w-[160px]"
+          onKeyDown={onKeyDown}
         >
-          <DotsHorizontalIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem onClick={() => router.push(`/edit-bill/${bill.id}`)}>
-          Edit
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          onKeyDown={(e) => {
-            console.log(e.key);
-            if (e.metaKey && e.key === "Delete") {
-              console.log("YIPPIE");
-            }
-          }}
-          onClick={() => console.log("DELETE UH YEAH")}
-        >
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem
+            onClick={() => router.push(`/edit-bill/${bill.id}`)}
+          >
+            Edit
+            {os === "MacOS" && <DropdownMenuShortcut>⌘E</DropdownMenuShortcut>}
+            {(os === "Windows" || os === "Linux") && (
+              <DropdownMenuShortcut>ctrl E</DropdownMenuShortcut>
+            )}
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            Delete
+            {os === "MacOS" && <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>}
+            {(os === "Windows" || os === "Linux") && (
+              <DropdownMenuShortcut>ctrl⌫</DropdownMenuShortcut>
+            )}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteDialog
+        open={open}
+        name={bill.name}
+        onOpenChange={setOpen}
+        action={() => console.log("DELETE?")}
+      />
+    </>
   );
 }
